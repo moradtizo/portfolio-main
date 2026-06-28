6cfb5601cff6d93f23bc60b2c93e97dcf79f15a1
moradtizo <moradtizo@gmail.com>
2026-06-28 21:35:57 +0100

    feat: replace PNG images with WEBP format for improved performance

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -31,28 +31,28 @@ export class ProjectsComponent {
     {
       title: 'Project 1',
       description: 'Movie Scope (Angular | TMDB API)',
-      imageUrl: '././.././../assets/project/P3.png',
+      imageUrl: '././.././../assets/project/P3.webp',
       websiteUrl: 'https://movie-mtizo.vercel.app/', // Replace with the actual URL
       hovered: false
     },
     {
       title: 'Project 2',
       description: 'Business website(UX/UI) Html & Css',
-      imageUrl: '././.././../assets/project/P4.png',
+      imageUrl: '././.././../assets/project/P4.webp',
       websiteUrl: 'https://business-mtizo.vercel.app/', // Replace with the actual URL
       hovered: false
     },
     {
       title: 'Project 3',
       description: 'Budget Management Platform',
-      imageUrl: '././.././../assets/project/P5.png',
+      imageUrl: '././.././../assets/project/P5.webp',
       websiteUrl: 'http://oddmaroc.com/pages-login', // Replace with the actual URL
       hovered: false
     },
     {
       title: 'e commerce website',
       description: 'E commerce website',
-      imageUrl: '././.././../assets/project/P6.png',
+      imageUrl: '././.././../assets/project/P6.webp',
       websiteUrl: 'https://example.com/project3', // Replace with the actual URL
       hovered: false
     },
